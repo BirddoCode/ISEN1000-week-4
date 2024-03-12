fc2194e5099923bc179c32ede52d34c94df1b829
Alden <90028114@study.curtincollege.edu.au>
2024-03-11 19:03:20 -0700

    B1 mycode file is edited to a new one

--- mycode.py
+++ mycode.py
@@ -1,10 +1,6 @@
 #
 # mycode.py: A simple demo code of ISEN1000
 #
-index = 100
-while index > -1:
-    print(index, end = ' ')
-    index -= 1
 print("This is a new part added")
 list1 = [1,2,3,4,5,6,7,8]
 for item in list1:
