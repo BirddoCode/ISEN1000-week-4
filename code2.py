87c613a4ce980b57cae0b761e630c7c25b0f07b9
Alden <90028114@study.curtincollege.edu.au>
2024-03-11 19:18:13 -0700

    Changing the same file with comments in on B2 and master branch will generate merge conflict

--- code2.py
+++ code2.py
@@ -1,12 +1,13 @@
 #
 # code2.py: A simple demo code of ISEN1000
 #
-index = 100
-while index > -1:
-    print(index, end = ' ')
+index = 100 # the index variable 
+while index > -1: # while loop 
+    print(index, end = ' ') # add printing and stuff
+    # index implementing 
     index -= 1
-print("This is a new part added")
-list1 = ['a', 'b', 'c', 'd', 'e','f','g']
-for item in list1:
-    print(item, end= ' ' )
-print("hello mr khuram")
+print("This is a new part added") # printing
+list1 = ['a', 'b', 'c', 'd', 'e','f','g']# printing list
+for item in list1: # list   
+    print(item, end= ' ' )# loop
+print("hello mr khuram") # printing
