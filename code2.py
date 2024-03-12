8201f61a3d41e90bcb26b756b2080167f0c479de
Alden <90028114@study.curtincollege.edu.au>
2024-03-11 18:44:33 -0700

    A new file has been added (duplicate)

---
+++ code2.py
@@ -0,0 +1,12 @@
+#
+# code2.py: A simple demo code of ISEN1000
+#
+index = 100
+while index > -1:
+    print(index, end = ' ')
+    index -= 1
+print("This is a new part added")
+list1 = ['a', 'b', 'c', 'd', 'e','f','g']
+for item in list1:
+    print(item, end= ' ' )
+print("hello mr khuram")
